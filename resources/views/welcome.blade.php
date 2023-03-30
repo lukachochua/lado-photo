77c01a35d83a24e21c847afc2d24f4aab162e5d7
lukachochua <lukachochua@gmail.com>
2023-03-30 23:58:54 +0400

    feat: add tailwind css to the project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lado Photo</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>LADO PHOTO</h1>
+</body>
+</html>
