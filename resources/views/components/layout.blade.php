08eca50d4d40a29cd0e48594cc0069a400a54eda
lukachochua <lukachochua@gmail.com>
2023-04-08 18:01:51 +0400

    refactor: slight refactor to tailwind css for popups  to work on smallers creens

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,19 +14,17 @@
   <header class="bg-gray-500 text-white">
     <nav class="flex justify-between items-center py-4 px-6">
       <div>
-        <a href="/" class="font-bold font-serif text-lg hover:bg-gray-700 px-4 py-2 rounded">Home</a>
+        <a href="/" class="font-extrabold text-lg hover:bg-gray-700 px-4 py-2 rounded">Home</a>
       </div>
       <div>
-        <a href="#" class="py-2 px-4 text-white font-bold hover:bg-gray-600 rounded-lg relative" onmouseover="showCategories()"
-          onmouseout="hideCategories()">
-          Photos 
+        <a href="#" class="py-2 px-4 text-white font-bold hover:bg-gray-600 rounded-lg relative"
+          onmouseover="showCategories()" onmouseout="hideCategories()">
+          Photos
         </a>
         <div id="categories" class="hidden absolute bg-gray-500 text-white w-auto py-2 mt-1 rounded-md z-10"
           onmouseover="showCategories()" onmouseout="hideCategories()">
           @foreach ($categories as $category)
-          <a 
-            href="{{ route('categories.show', ['category' => $category->id]) }}"
-            class="block px-4 py-2 text-gray-300 hover:bg-gray-600 hover:text-white font-bold focus:bg-gray-700
+          <a href="{{ route('categories.show', ['category' => $category->id]) }}" class="block px-4 py-2 text-gray-300 hover:bg-gray-600 hover:text-white font-bold focus:bg-gray-700
             focus:text-white rounded-md transition-colors duration-300">
             {{ $category->name }}
           </a>
@@ -36,11 +34,14 @@
         <a href="#" class="py-2 px-4 text-white font-bold hover:bg-gray-700 rounded-lg">Contact</a>
       </div>
     </nav>
-    <div class="bg-gray-200 min-h-screen pt-10 pb-6 overflow-hidden">
-      <div class="max-w-screen-lg mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2">
-        {{ $slot }}
+
+    <main>
+      <div class="bg-gray-200 min-h-screen pt-10 pb-6 overflow-hidden">
+        <div class="max-w-screen-lg mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2">
+          {{ $slot }}
+        </div>
       </div>
-    </div>
+    </main>
   </header>
 </body>
 
--- resources/views/components/popup.blade.php
+++ resources/views/components/popup.blade.php
@@ -1,6 +1,6 @@
 <div id="popup"
-    class="hidden fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-90 flex justify-center items-center z-20">
-    <div class="relative border border-gray-500  h-screen w-3/5">
+    class="hidden fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-80 justify-center items-center z-20">
+    <div class="relative lg:w-3/5 lg:h-full w-full h-auto border border-gray-500">
         <img src="" alt="Your Image" id="popup-image" class="w-full h-full object-cover">
         <button
             class="absolute bg-opacity-60 top-2 right-2 text-white font-extrabold text-xl cursor-pointer rounded-full bg-red-500 hover:bg-red-600 p-2"
