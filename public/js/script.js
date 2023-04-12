dae6aac69cd1200fda8c8ba970b57ab5a06e773e
lukachochua <lukachochua@gmail.com>
2023-04-12 20:31:11 +0400

    style: refactor  styles for the smaller screens

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,49 @@
-function showCategories() {
-  document.getElementById("categories").style.display = "block";
-}
+if (window.innerWidth >= 768) {
+  function showCategories() {
+    document.getElementById("categories").style.display = "block";
+  }
 
-function hideCategories() {
-  document.getElementById("categories").style.display = "none";
-}
+  function hideCategories() {
+    document.getElementById("categories").style.display = "none";
+  }
 
-function showPopup(imageUrl) {
-  var popup = document.getElementById("popup");
-  var popupImage = document.getElementById("popup-image");
-  popupImage.src = imageUrl;
-  popup.style.display = "flex";
-}
+  function showPopup(imageUrl) {
+    var popup = document.getElementById("popup");
+    var popupImage = document.getElementById("popup-image");
+    popupImage.src = imageUrl;
+    popup.style.display = "flex";
+  }
 
-function hidePopup() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "none";
-}
-
-document.addEventListener('click', function (event) {
-  var popup = document.getElementById("popup");
-  if (event.target == popup) {
+  function hidePopup() {
+    var popup = document.getElementById("popup");
     popup.style.display = "none";
   }
-});
 
-document.addEventListener('keydown', function (event) {
-  var popup = document.getElementById("popup");
-  if (event.key === "Escape" && popup.style.display === "flex") {
-    popup.style.display = "none";
+
+  document.addEventListener('click', function (event) {
+    var popup = document.getElementById("popup");
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  });
+  document.addEventListener('keydown', function (event) {
+    var popup = document.getElementById("popup");
+    if (event.key === "Escape" && popup.style.display === "flex") {
+      popup.style.display = "none";
+    }
+  });
+}
+
+if (window.innerWidth <= 768) {
+  function showCategories() {
+    document.getElementById("categories").style.display = "block";
   }
-});
+
+  function hideCategories() {
+    document.getElementById("categories").style.display = "none";
+  }
+}
+
+
+
+
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,24 +1,27 @@
-
 <nav class="flex justify-between items-center py-4 px-6 font-sans">
     <div>
         <a href="/"
-            class="py-2 px-4 text-white font-bold hover:text-gray-500 hover:border-t-2 border-gray-500 relative transition duration-400 ease-in-out text-lg">HOME</a>
+            class="hidden sm:inline py-2 px-4 text-white font-bold hover:text-gray-500 hover:border-t-2 border-gray-500 relative transition duration-400 ease-in-out text-lg">HOME</a>
     </div>
-    <div>
+
+    <div class="absolute sm:relative inset-0">
         <x-button href="/" onmouseover="showCategories()" onmouseout="hideCategories()">
             PHOTOS
         </x-button>
-        <div id="categories" class="hidden absolute bg-gray-800 text-white w-auto py-2 mt-1 rounded-md z-10"
+
+        <div id="categories" class="hidden absolute bg-gray-800 text-white w-auto py-2 mt-1 z-10"
             onmouseover="showCategories()" onmouseout="hideCategories()">
             @foreach ($categories as $category)
-                <x-button href="{{ route('categories.show', ['category' => $category->id]) }}"
-                    class="block hover:border-l-4" :withoutClasses="['hover:border-t-2', 'text-lg']">
-                    {{ strtoupper($category->name) }}
-                </x-button>
+            <x-button href="{{ route('categories.show', ['category' => $category->id]) }}"
+                class="block hover:border-l-4" :withoutClasses="['hover:border-t-2', 'text-lg']">
+                {{ strtoupper($category->name) }}
+            </x-button>
             @endforeach
         </div>
+
         <x-button href="videos">VIDEOS</x-button>
         <x-button href="#">ABOUT</x-button>
         <x-button href="#">CONTACT</x-button>
     </div>
+
 </nav>
