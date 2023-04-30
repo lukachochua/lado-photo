51b175e46c71ce16f05639b4303992a412fdc35d
lukachochua <lukachochua@gmail.com>
2023-04-30 18:37:15 +0400

    feat: change video page design, add favicon

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,57 +1,6 @@
 <x-layout :categories="$categories">
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            frameborder="1"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <div>
-            <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-                title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen></iframe>
-        </div>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </div>
-    <div>
-        <iframe class="w-full h-full" src="https://www.youtube.com/embed/_wekTHKXffo"
-            title="#ტელეპონტი | @FoxyEleniko  &amp; @Octopusi" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
+    <div class="flex justify-center items-center">
+      <h1 class="text-2xl bold font-bold text-black leading-relaxed p-4 hover:text-gray-800 select-none h-30 overflow-hidden">Coming Soon...</h1>
     </div>
 </x-layout>
+
