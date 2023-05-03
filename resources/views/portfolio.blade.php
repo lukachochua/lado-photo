0e2cda1f49ea8edc75e03a0f99583707b598b714
lukachochua <lukachochua@gmail.com>
2023-05-03 21:00:36 +0400

    feat: add responsiveness to the portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -3,13 +3,13 @@
         <div class="flex flex-wrap justify-center w-200">
             @foreach ($posts as $post)
                 @if ($post->aspectRatio < 1) 
-                    <div class="flex mb-4 items-center justify-center w-1/2" id="vertical">
-                        <img src="{{ asset("storage/images/{$post->photo}") }}" class="sm:w-100 w-60 shadow-xl object-contain
-                        rounded-lg mr-1">
+                    <div class="flex mb-4 items-center justify-center sm:w-2/5 mx-1" id="vertical">
+                        <img src="{{ asset("storage/images/{$post->photo}") }}" class="shadow-xl object-contain
+                        rounded-lg">
                     </div>
                 @else
                     <div class="flex mb-4 justify-center w-full" id="horizontal">
-                        <img src="{{ asset("storage/images/{$post->photo}") }}" class="sm:w-200 shadow-xl object-cover rounded-lg
+                    <img src="{{ asset("storage/images/{$post->photo}") }}" class="sm:w-100 w-96 shadow-xl object-contain rounded-lg
                         mx-auto">
                     </div>
                 @endif
